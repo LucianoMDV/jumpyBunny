94c87c033acdfc8329a38adc713a7264010af5ed
Luciano D'Agata <martinvilla481@gmail.com>
2021-01-16 22:19:29 -0300

    add player and playerController

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start our game
+   public void Start()
+    {
+        
+    }
+
+    // Called when player dies
+    public void Update()
+    {
+        
+    }
+    
+    //Called when the player decide to quick the game
+    //and go to the menu
+    public void BackToMainMenu() {}
+}
